3359364535be0d2d5bcea636e2fab5162abacec4
Suresh <sureshcbt@gmail.com>
2019-10-11 13:26:13 -0400

    Listed the proteins alphabetically by adding ordering option

--- database/admin.py
+++ database/admin.py
@@ -7,6 +7,7 @@ class PesticidalProteinDatabaseAdmin(admin.ModelAdmin):
     search_fields = ('name', 'oldname', 'accession','year')
     fields = ('name', 'oldname', 'accession','year')
     list_display = ('name','oldname', 'accession','year',)
+    ordering = ('name',)
 
 
 admin.site.register(PesticidalProteinDatabase, PesticidalProteinDatabaseAdmin)
