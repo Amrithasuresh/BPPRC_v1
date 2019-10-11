48c270a18f73aded5bbc1f3b770517b961901ea1
Suresh <sureshcbt@gmail.com>
2019-10-11 13:51:08 -0400

    Removed bestmatchfinder

--- BPPRC/urls.py
+++ BPPRC/urls.py
@@ -5,5 +5,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('database.urls')),
-    path('', include('bestmatchfinder.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
