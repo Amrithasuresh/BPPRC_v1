14703ff59ea9d7c975ea9bba5ac04d82e702bd26
Suresh <sureshcbt@gmail.com>
2019-10-11 13:25:02 -0400

    Added list display in the admin file

--- database/admin.py
+++ database/admin.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
 from .models import PesticidalProteinDatabase, \
-    PesticidalProteinDatabaseAdmin, Description
-
-
-admin.site.register(PesticidalProteinDatabase,
-                    PesticidalProteinDatabaseAdmin)
+    Description
 
 
 class PesticidalProteinDatabaseAdmin(admin.ModelAdmin):
     search_fields = ('name', 'oldname', 'accession','year')
+    fields = ('name', 'oldname', 'accession','year')
+    list_display = ('name','oldname', 'accession','year',)
 
-    def get_ordering(self, request):
-        return('name')
 
-
-@admin.register(Description)
-class DefaultAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(PesticidalProteinDatabase, PesticidalProteinDatabaseAdmin)
